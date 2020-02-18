2502f816f60c3dae884b199255b9df8cc2512182
alfaddin <61052038+alfaddin@users.noreply.github.com>
2020-02-18 21:06:43 +0400

    Delete script.sh

--- script.sh
+++
@@ -1,6 +0,0 @@
-mysql -u root
-create database drupal;
-create user 'alfaddin'@'localhost' identified by '12345';
-grant all privileges on drupal.* to 'alfaddin'@'localhost' identified by '12345';
-flush privileges;
-quit;
